7ae9472e492a76507a412b294a29c29c4408c97c
Kunal Yadav <55584238+damncoolkunal@users.noreply.github.com>
2020-08-10 02:26:10 +0530

    Update object_tracker.py

--- object_tracker.py
+++ object_tracker.py
@@ -1,7 +1,4 @@
-# USAGE
-# python object_tracker.py --prototxt deploy.prototxt --model res10_300x300_ssd_iter_140000.caffemodel
 
-# import the necessary packages
 from pyimagesearch.centroidtracker import CentroidTracker
 from imutils.video import VideoStream
 import numpy as np
@@ -9,8 +6,8 @@ import argparse
 import imutils
 import time
 import cv2
-
 # construct the argument parse and parse the arguments
+
 ap = argparse.ArgumentParser()
 ap.add_argument("-p", "--prototxt", required=True,
 	help="path to Caffe 'deploy' prototxt file")
@@ -43,9 +40,7 @@ while True:
 	if W is None or H is None:
 		(H, W) = frame.shape[:2]
 
-	# construct a blob from the frame, pass it through the network,
-	# obtain our output predictions, and initialize the list of
-	# bounding box rectangles
+	
 	blob = cv2.dnn.blobFromImage(frame, 1.0, (W, H),
 		(104.0, 177.0, 123.0))
 	net.setInput(blob)
@@ -54,41 +49,36 @@ while True:
 
 	# loop over the detections
 	for i in range(0, detections.shape[2]):
-		# filter out weak detections by ensuring the predicted
-		# probability is greater than a minimum threshold
+		
 		if detections[0, 0, i, 2] > args["confidence"]:
-			# compute the (x, y)-coordinates of the bounding box for
-			# the object, then update the bounding box rectangles list
+			
 			box = detections[0, 0, i, 3:7] * np.array([W, H, W, H])
 			rects.append(box.astype("int"))
 
-			# draw a bounding box surrounding the object so we can
-			# visualize it
+			
 			(startX, startY, endX, endY) = box.astype("int")
 			cv2.rectangle(frame, (startX, startY), (endX, endY),
 				(0, 255, 0), 2)
 
-	# update our centroid tracker using the computed set of bounding
-	# box rectangles
+	
 	objects = ct.update(rects)
 
-	# loop over the tracked objects
+	
 	for (objectID, centroid) in objects.items():
-		# draw both the ID of the object and the centroid of the
-		# object on the output frame
+		
 		text = "ID {}".format(objectID)
 		cv2.putText(frame, text, (centroid[0] - 10, centroid[1] - 10),
 			cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2)
 		cv2.circle(frame, (centroid[0], centroid[1]), 4, (0, 255, 0), -1)
 
-	# show the output frame
+	
 	cv2.imshow("Frame", frame)
 	key = cv2.waitKey(1) & 0xFF
 
-	# if the `q` key was pressed, break from the loop
+	
 	if key == ord("q"):
 		break
 
-# do a bit of cleanup
+
 cv2.destroyAllWindows()
 vs.stop()
